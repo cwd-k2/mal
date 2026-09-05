82fdb55387c218a5615867d43ef4a8cd33cb0f21
cwd-k2 <kaito.augmented7th@gmail.com>
2026-09-05 06:41:11 +0000

    refactor: isolate Bool lowering

--- compiler/src/core.rs
+++ compiler/src/core.rs
@@ -4,9 +4,11 @@ use crate::resolve::ast::{FALSE_VALUE, TRUE_VALUE};
 use crate::source::Span;
 
 pub mod ast;
+mod bool;
 mod pattern;
 mod primitive;
 
+use self::bool::bool_type;
 use self::primitive::lower_binary_primitive;
 
 use self::ast::{
@@ -296,131 +298,6 @@ impl Lowerer {
         body
     }
 
-    fn lower_if(
-        &mut self,
-        condition: &checked::Expression,
-        then_branch: &checked::ExpressionBlock,
-        else_branch: &checked::ExpressionBlock,
-        result_type: &checked::Type,
-        span: Span,
-    ) -> Expression {
-        let otherwise = self.lower_body(&else_branch.items, &else_branch.result);
-        let then = self.lower_body(&then_branch.items, &then_branch.result);
-        let condition = self.lower_expression(condition);
-        self.case(
-            condition,
-            vec![
-                self.wildcard_arm(0, otherwise, else_branch.span),
-                self.wildcard_arm(1, then, then_branch.span),
-            ],
-            result_type.clone(),
-            span,
-        )
-    }
-
-    fn lower_logical_not(&mut self, operand: &checked::Expression, span: Span) -> Expression {
-        let scrutinee = self.lower_expression(operand);
-        let false_value = self.bool_value(false, span);
-        let true_value = self.bool_value(true, span);
-        self.case(
-            scrutinee,
-            vec![
-                self.wildcard_arm(0, true_value, span),
-                self.wildcard_arm(1, false_value, span),
-            ],
-            bool_type(),
-            span,
-        )
-    }
-
-    fn lower_short_circuit(
-        &mut self,
-        operator: BinaryOperator,
-        left: &checked::Expression,
-        right: &checked::Expression,
-        span: Span,
-    ) -> Expression {
-        let left = self.lower_expression(left);
-        let right = self.lower_expression(right);
-        let false_value = self.bool_value(false, span);
-        let true_value = self.bool_value(true, span);
-        let arms = match operator {
-            BinaryOperator::LogicalAnd => vec![
-                self.wildcard_arm(0, false_value, span),
-                self.wildcard_arm(1, right, span),
-            ],
-            BinaryOperator::LogicalOr => vec![
-                self.wildcard_arm(0, right, span),
-                self.wildcard_arm(1, true_value, span),
-            ],
-            _ => unreachable!("caller restricts short-circuit operators"),
-        };
-        self.case(left, arms, bool_type(), span)
-    }
-
-    fn lower_bool_equality(
-        &mut self,
-        operator: BinaryOperator,
-        left: &checked::Expression,
-        right: &checked::Expression,
-        span: Span,
-    ) -> Expression {
-        let left_id = self.temporary();
-        let right_id = self.temporary();
-        let equal = operator == BinaryOperator::Equal;
-        let right_when_false = self.bool_case_reference(right_id, equal, !equal, span);
-        let right_when_true = self.bool_case_reference(right_id, !equal, equal, span);
-        let comparison = self.case(
-            self.reference(left_id, bool_type(), span),
-            vec![
-                self.wildcard_arm(0, right_when_false, span),
-                self.wildcard_arm(1, right_when_true, span),
-            ],
-            bool_type(),
-            span,
-        );
-        let right = self.lower_expression(right);
-        let right_let = self.temporary_let(right_id, right, comparison, span);
-        let left = self.lower_expression(left);
-        self.temporary_let(left_id, left, right_let, span)
-    }
-
-    fn bool_case_reference(&self, id: ValueId, zero: bool, one: bool, span: Span) -> Expression {
-        self.case(
-            self.reference(id, bool_type(), span),
-            vec![
-                self.wildcard_arm(0, self.bool_value(zero, span), span),
-                self.wildcard_arm(1, self.bool_value(one, span), span),
-            ],
-            bool_type(),
-            span,
-        )
-    }
-
-    fn temporary_let(
-        &self,
-        id: ValueId,
-        value: Expression,
-        body: Expression,
-        span: Span,
-    ) -> Expression {
-        Expression {
-            kind: ExpressionKind::Let {
-                binding: Box::new(Binding {
-                    pattern: Pattern::Binding {
-                        id,
-                        ty: value.ty.clone(),
-                    },
-                    value,
-                    span,
-                }),
-                body: Box::new(body),
-            },
-            ty: bool_type(),
-            span,
-        }
-    }
-
     fn lower_case_arm(&mut self, arm: &checked::CaseArm) -> CaseArm {
         CaseArm {
             index: arm.index,
@@ -459,21 +336,6 @@ impl Lowerer {
         }
     }
 
-    fn bool_value(&self, value: bool, span: Span) -> Expression {
-        Expression {
-            kind: ExpressionKind::SumInjection {
-                index: usize::from(value),
-                value: Box::new(Expression {
-                    kind: ExpressionKind::Unit,
-                    ty: checked::Type::Unit,
-                    span,
-                }),
-            },
-            ty: bool_type(),
-            span,
-        }
-    }
-
     fn reference(&self, id: ValueId, ty: checked::Type, span: Span) -> Expression {
         Expression {
             kind: ExpressionKind::Reference(id),
@@ -487,8 +349,4 @@ impl Lowerer {
         self.next_temporary += 1;
         id
     }
-}
-
-fn bool_type() -> checked::Type {
-    checked::Type::Sum(vec![checked::Type::Unit, checked::Type::Unit])
 }
